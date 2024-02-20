921f12be6834f0fc17157870b6c2c837297fbd01
Jugveer-Sandher <jugveersandher@gmail.com>
2024-02-19 19:34:31 -0800

    implemented ordering job list

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="job-list">
+        <p>Ordered by {{ order }}</p>
         <ul>
-            <li v-for="job in jobs" :key="job.id">
+            <li v-for="job in orderedJobs" :key="job.id">
                 <h2> {{ job.title }} in {{ job.location }}</h2>
                 <div class="salary">
                     <p>{{ job.salary }} rupees</p>
@@ -16,14 +17,29 @@
 
 <script lang="ts">
 import Job from '@/types/Job'
-import { defineComponent, PropType } from 'vue'
+import OrderTerm from '@/types/OrderTerm'
+import { defineComponent, PropType, computed } from 'vue'
 
 export default defineComponent({
     props: {
         jobs: {
             required: true,
             type: Array as PropType<Job[]>
+        },
+        order: {
+            required: true,
+            type: String as PropType<OrderTerm>
         }
+    },
+
+    setup(props) {
+        const orderedJobs = computed(() => {
+            return [...props.jobs].sort((a: Job, b: Job) => {
+                return a[props.order] > b[props.order] ? 1 : -1
+            })
+        })
+
+        return { orderedJobs }
     }
 })
 </script>
